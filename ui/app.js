6df463c4cf6b47b6fb3bf33a7fd7d5627d88a789
africano <africanokevin3@gmail.com>
2019-05-10 10:11:28 -0700

    [feature] ui resource files

---
+++ ui/app.js
@@ -0,0 +1,11 @@
+document.getElementById('date').innerHTML=new Date().toDateString()
+
+function fFunction(){
+  var a= document.getElementById('fname')
+  a.value=a.value.toUpperCase();
+}
+
+function lFunction(){
+  var a= document.getElementById('lname')
+  a.value=a.value.toUpperCase();
+}
